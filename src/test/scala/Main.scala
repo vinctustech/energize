a114981a82e841cdd054be8cf6de8b76e60a0870
Edward A Maxedon Sr <edadma@gmail.com>
2016-11-23 20:55:43 -0500

    create table command

--- src/test/scala/Main.scala
+++ src/test/scala/Main.scala
@@ -1,6 +1,6 @@
 package xyz.hyperreal.informatio
 
-import java.io.FileReader
+import java.sql._
 
 import collection.immutable.PagedSeq
 import util.parsing.input.{PagedSeqReader, CharSequenceReader}
@@ -12,7 +12,7 @@ object Main extends App {
 	
 	val p = new InformatioParser
 	val ast =
-		p.parse( new CharSequenceReader(io.Source.fromFile("t1.info").getLines.map(l => l + '\n').mkString) ) match {
+		p.parse( new CharSequenceReader(io.Source.fromFile("t0.info").getLines.map(l => l + '\n').mkString) ) match {
 			case p.Success( tree, _ ) => tree
 			case p.NoSuccess( error, rest ) =>
 				println( rest.pos.line + ": " + error + "\n" + rest.pos.longString )
@@ -20,9 +20,39 @@ object Main extends App {
 				sys.error( "" )
 		}
 
+		println( ast )
+	Class.forName( "org.h2.Driver" )
+	
+	val conn = DriverManager.getConnection( "jdbc:h2:~/projects/informatio/test", "sa", "" )
+	
+	def problem( error: String ) {
+		conn.close
+		sys.error( error )
+	}
+	
 	ast foreach {
 		case TableDefinition( name, fields ) =>
-			
+			if (!conn.getMetaData.getTables( null, null, name, null ).next) {
+				val f =
+					fields map {
+						case TableField( modifiers, typ, name ) =>
+							val t =
+								typ match {
+									case StringType => "VARCHAR(255)"
+									case UUIDType => "UUID"
+									case DateType => "DATE"
+								}
+								
+							name + " " + t
+					} mkString ", "
+				
+				val com = "CREATE TABLE " + name + "(" + "id INT AUTO_INCREMENT PRIMARY KEY, " + f + ")"
+				
+				println( com )
+			}
 		case RoutesDefinition( base, mappings ) =>
+			
 	}
+	
+	conn.close
 }
