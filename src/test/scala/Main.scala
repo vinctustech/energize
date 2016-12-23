5589f9dfc279d479a1861275f1a46d0f8c558624
Edward A Maxedon, Sr <edadma@gmail.com>
2016-12-23 13:56:04 -0500

    sample customers table

--- src/test/scala/Main.scala
+++ src/test/scala/Main.scala
@@ -5,12 +5,18 @@ import java.io.File
 
 object Main extends App {
 
-	val (c, s) = dbconnect( "books", true )
-	val env = configure( io.Source.fromFile("books.cras"), c, s )
+// 	val (c, s) = dbconnect( "books", true )
+// 	val env = configure( io.Source.fromFile("books.cras"), c, s )
+// 	
+// //  	println( env.process("GET", "/books?filter=title=The+Adventures+of+Huckleberry+Finn,books.id=3", null) )
+//  	println( env.process("GET", "/books?order=title:asc", null) )
+// 	println( env.process("GET", "/books/1", null) )
+
+	val (c, s) = dbconnect( "customers", true )
+	val env = configure( io.Source.fromFile("customers.cras"), c, s )
 	
-//  	println( env.process("GET", "/books?filter=title=The+Adventures+of+Huckleberry+Finn,books.id=3", null) )
- 	println( env.process("GET", "/books?order=title:asc", null) )
-	println( env.process("GET", "/books/1", null) )
+//  	println( env.process("GET", "/customers?filter=title=The+Adventures+of+Huckleberry+Finn,books.id=3", null) )
+ 	println( env.process("GET", "/customers?order=CustomerName:desc", null) )
 	
 	c.close
 }
