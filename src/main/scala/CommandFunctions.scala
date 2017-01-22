ac732a282943301313d1f314c693f330ed04af34
Edward A Maxedon <edadma@gmail.com>
2017-01-22 13:38:53 -0500

    fix update() to allow PUTing into a resource with a many-to-many field; add 'authorization' config block

--- src/main/scala/CommandFunctions.scala
+++ src/main/scala/CommandFunctions.scala
@@ -134,8 +134,8 @@ object CommandFunctions {
 	}
 	
 	def update( env: Env, resource: Table, id: Long, json: OBJ, all: Boolean ) =
-		if (all && json.keySet != resource.names.toSet)
-			throw new CrasErrorException( "update: missing column(s) in PUT request" )
+		if (all && json.keySet != (resource.names.toSet filterNot (n => resource.columns(env.db.desensitize(n)).typ.isInstanceOf[ManyReferenceType])))
+			throw new CrasErrorException( "update: missing column(s): " + (resource.names.toSet -- json.keySet).mkString(", ") )
 		else {
 			val com = new StringBuilder( "UPDATE " )
 			var typ: ColumnType = null
