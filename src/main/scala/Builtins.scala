f739be7aeed62fb22626222c58fd7d6260d1bca3
Edward A Maxedon <edadma@gmail.com>
2017-01-22 12:28:24 -0500

    many-to-many routes only get added if there is a many-to-many field

--- src/main/scala/Builtins.scala
+++ src/main/scala/Builtins.scala
@@ -25,25 +25,21 @@ object Builtins {
 		"""
 		|route <base>/<resource>
 		|  GET     /id:long                  dataResult( "<resource>", singleOrNotFound(findID(<resource>, id, ?fields, None, None, None)) )
-		|  GET     /id:long/field:           dataResult( "<resource>", singleOrNotFound(findIDMany(<resource>, id, field, ?page, ?start, ?limit)) )
 		|  GET     /                         dataResult( "<resource>", list(<resource>, ?fields, ?filter, ?order, ?page, ?start, ?limit) )
 		|  POST    /                         dataResult( "<resource>", insert(<resource>, json) )
-		|  POST    /id:long/field:           dataResult( "<resource>", insertLinks(<resource>, id, field, json) )
 		|  PATCH   /id:long                  atLeastOneOrNotFound( update(<resource>, id, json, false) ); null
 		|  PUT     /id:long                  atLeastOneOrNotFound( update(<resource>, id, json, true) ); null
 		|  DELETE  /id:long                  atLeastOneOrNotFound( delete(<resource>, id) ); null
+		""".stripMargin
+
+	val mtmroutes =
+		"""
+		|route <base>/<resource>
+		|  GET     /id:long/field:           dataResult( "<resource>", singleOrNotFound(findIDMany(<resource>, id, field, ?page, ?start, ?limit)) )
+		|  POST    /id:long/field:           dataResult( "<resource>", insertLinks(<resource>, id, field, json) )
 		|  DELETE  /id:long/field:           deleteLinks( <resource>, id, field, json )
 		|  DELETE  /id:long/field:/tid:long  atLeastOneOrNotFound( deleteLinksID(<resource>, id, field, tid) ); null
 		""".stripMargin
-
-//	val mtmroutes =
-//		"""
-//		|route <base>/<resource>
-//		|  GET     /id:long  dataResult( "<resource>", singleOrNotFound(findID(<resource>, id, ?fields)) )
-//		|  GET     /         dataResult( "<resource>", list(<resource>, ?fields, ?filter, ?order, ?page, ?start, ?limit) )
-//		|  POST    /         dataResult( "<resource>", insert(<resource>, json) )
-//		|  DELETE  /id:long  atLeastOneOrNotFound( delete(<resource>, id) ); null
-//		""".stripMargin
 
 	val control = io.Source.fromString(
 		"""
--- src/main/scala/Cras.scala
+++ src/main/scala/Cras.scala
@@ -141,13 +141,26 @@ object Cras {
 		
 					tables(db.desensitize( name )) = Table( name, cols map {case (_, cinfo) => cinfo.name} toList, cols.toMap, resource, null )
 
-					if (resource)
-						if (bases isEmpty)
-							routes ++= configure( io.Source.fromString( Builtins.routes.replaceAll( "<base>", "" ).replaceAll( "<resource>", name ) ), null, null ).routes
-						else
-							for (URIPath( base ) <- bases)
+					if (resource) {
+						val mtm = cols exists {case (_, Column(_, typ, _, _, _, _)) => typ.isInstanceOf[ManyReferenceType]}
+
+						if (bases isEmpty) {
+							routes ++= configure( io.Source.fromString( Builtins.routes.replaceAll( "<base>", "" ).
+								replaceAll( "<resource>", name ) ), null, null ).routes
+
+							if (mtm)
+								routes ++= configure( io.Source.fromString( Builtins.mtmroutes.replaceAll( "<base>", "" ).
+									replaceAll( "<resource>", name ) ), null, null ).routes
+						} else
+							for (URIPath( base ) <- bases) {
 								routes ++= configure( io.Source.fromString( Builtins.routes.replaceAll( "<resource>", name ).
 									replaceAll( "<base>", base map { case NameURISegment( segment ) => segment } mkString("/", "/", "") ) ), null, null ).routes
+
+								if (mtm)
+									routes ++= configure( io.Source.fromString( Builtins.mtmroutes.replaceAll( "<resource>", name ).
+										replaceAll( "<base>", base map { case NameURISegment( segment ) => segment } mkString("/", "/", "") ) ), null, null ).routes
+							}
+					}
 
 				case RoutesDefinition( URIPath(base), mappings ) =>
 					mappings foreach {
