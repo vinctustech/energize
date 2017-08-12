96eee1ebfe377af7abed64de9b103ac2205cdba9
Edward A Maxedon <edadma@gmail.com>
2017-08-12 16:50:26 -0400

    start coding field rename

--- src/main/scala/Environment.scala
+++ src/main/scala/Environment.scala
@@ -385,8 +385,8 @@ class Environment( val tables: Map[String, Table], croutes: List[Route], val bin
 
 case class Route( method: String, path: URIPath, action: ExpressionAST )
 
-case class Table( name: String, columns: List[Column], columnMap: Map[String, Column], resource: Boolean, base: Option[URIPath], mtm: Boolean, ma: Boolean,
-									var preparedInsert: PreparedStatement, var preparedFullInsert: PreparedStatement ) {
+case class Table( var name: String, columns: List[Column], columnMap: Map[String, Column], resource: Boolean, base: Option[URIPath], mtm: Boolean,
+									ma: Boolean, var preparedInsert: PreparedStatement, var preparedFullInsert: PreparedStatement ) {
 	def names = columns map (_.name)
 }
 
