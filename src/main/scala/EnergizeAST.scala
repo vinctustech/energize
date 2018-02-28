3cc818c3d722b6b7267556a22579d3e4aa5866f7
Edward A Maxedon Sr <edadma@gmail.com>
2018-02-28 13:58:13 -0500

    remove junk

--- src/main/scala/EnergizeAST.scala
+++ src/main/scala/EnergizeAST.scala
@@ -7,7 +7,6 @@ import scala.util.parsing.input.Position
 
 case class EnumDefinition( pos: Position, name: String, enum: List[(Position, String)] ) extends DeclarationStatementAST
 case class RealmDefinition( pos: Position, realm: String ) extends DeclarationStatementAST
-//case class BaseDeclaration( path: PathSegment ) extends DeclarationStatementAST
 case class RoutesDefinition( pos: Position, base: Option[PathSegment], protection: Option[Option[String]],
 														 mappings: Seq[RouteMapping] ) extends DeclarationStatementAST
 
@@ -26,12 +25,6 @@ case class OptionalPathSegment( seg: PathSegment ) extends PathSegment
 case class OneOrMorePathSegment( seg: PathSegment ) extends PathSegment
 case class RepeatPathSegment( subpat: PathSegment, lower: Int, pos: Position, upper: Option[Int] ) extends PathSegment
 
-//trait ResourceField
-//case class ArrayResourceField( pos: Position, name: String, typ: FieldTypeAST ) extends ResourceField
-//case class SimpleResourceField( pos: Position, name: String, typ: FieldTypeAST ) extends ResourceField
-//
-//case class FieldTypeAST( pos: Position, name: String, args: Seq[Any] )
-
 case class RouteMapping( pos: Position, method: String, path: PathSegment, guard: Option[ExpressionAST], action: ExpressionAST )
 
 case class ResourceDefinition( protection: Option[Option[String]], pos: Position, name: String, base: Option[PathSegment],
@@ -39,32 +32,3 @@ case class ResourceDefinition( protection: Option[Option[String]], pos: Position
 
 case class ResourceField( pos: Position, name: String, tpos: Position, tname: String, args: List[Any],
 													modifiers: List[(Position, String)], array: Boolean )
-
-/*
-trait ColumnType extends Positional
-trait PrimitiveColumnType extends ColumnType
-case object BooleanType extends PrimitiveColumnType
-case object StringType extends PrimitiveColumnType
-case object TextType extends PrimitiveColumnType
-case object IntegerType extends PrimitiveColumnType
-case object LongType extends PrimitiveColumnType
-case object UUIDType extends PrimitiveColumnType
-case object DateType extends PrimitiveColumnType
-case object DatetimeType extends PrimitiveColumnType
-case object TimeType extends PrimitiveColumnType
-case object TimestampType extends PrimitiveColumnType
-case object TimestamptzType extends PrimitiveColumnType
-case object BinaryType extends PrimitiveColumnType
-case class BLOBType( rep: Symbol ) extends PrimitiveColumnType
-case object FloatType extends PrimitiveColumnType
-case class DecimalType( prec: Int, scale: Int ) extends PrimitiveColumnType
-case class MediaType( allowed: List[MimeType], limit0: String, var limit: Int ) extends PrimitiveColumnType
-case class ArrayType( parm: PrimitiveColumnType, dpos: Position, dim: String, var dimint: Int ) extends ColumnType
-case class SingleReferenceType( table: String, var ref: Table ) extends ColumnType with ReferenceType
-case class ManyReferenceType( table: String, var ref: Table ) extends ColumnType with ReferenceType
-case class EnumType( name: String, enum: Vector[String] ) extends PrimitiveColumnType
-case class IdentType( ident: String ) extends ColumnType
-
-case class MimeType( typ: String, subtype: String )
-*/
-
