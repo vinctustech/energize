3ce3c6a276006e61f2dc1dac819f7c3e200118e2
Edward A Maxedon <edadma@gmail.com>
2018-04-28 18:57:41 -0400

    fix

--- src/main/scala/SiteFunctions.scala
+++ src/main/scala/SiteFunctions.scala
@@ -33,8 +33,8 @@ object SiteFunctionHelpters {
 object SiteFunctions {
 
   def folderExistsUnderDocroot( vm: VM, path: String ) = {
-    val file = new File( SiteFunctionHelpters.docroot, path )
     val root = new File( SiteFunctionHelpters.docroot )
+    val file = new File( root, path )
 
     if (file.getCanonicalPath == root.getCanonicalPath ||
       file.getParentFile.getCanonicalPath == root.getCanonicalPath ||
