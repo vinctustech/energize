1faa6a1f163eccfe6d0d8f08e6735122dcb95424
Edward A Maxedon Sr <edadma@gmail.com>
2018-06-07 11:47:44 -0400

    fix requestJSON to use 'Accept: application/json' header

--- src/main/scala/ClientFunctions.scala
+++ src/main/scala/ClientFunctions.scala
@@ -12,6 +12,7 @@ object ClientFunctions {
   def requestJSON( vm: VM, url: String ): Any =
     DefaultJSONReader.fromString(
       Request.Get(url)
+        .addHeader( "Accept", "application/json" )
         .connectTimeout(1000)
         .socketTimeout(1000)
         .execute.returnContent.asString )
